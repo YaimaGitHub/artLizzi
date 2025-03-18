53e33696ec29cea401a3a48114f8b24d5901afc4
YaimaGitHub <tvalacarta33@gmail.com>
2025-03-18 05:54:37 -0400

    ok
    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Bienvenido | Lizzi Nails";
+            document.title = "Bienvenido | Lizziy's Nails";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
